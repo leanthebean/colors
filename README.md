e172a0d513bead6b5e9c49b2fbb76b718f425a1d
leanthebean <elena@vt.edu>
2018-02-09 17:48:37 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# erc721_token_tutorial
-erc721 token tutorial for the eth denver workshop
+# ERC-721 Token Tutorial - Color Tokens!
+
+### Set Up 
+First, make sure you have nodejs v8, git, and metamask installed    
+- Node.js v8 (and npm) from https://nodejs.org/ (check your version by typing “node -v” in your terminal, if it’s below 7.6, please upgrade)
+- Git from https://git-scm.com/ 
+- Metamask from https://metamask.io/ 
+
+Next up, start installing some global dependencies. We'll need testrpc and truffle for this tutorial   
+- `npm install -g ethereumjs-testrpc`
+- `npm install -g truffle`
+
+Next is to clone/download this repository and install all the node dependencies
+1. `git clone git@github.com:leanthebean/colors.git`
+2. `cd colors`
+3. `npm install`
+
+### Using Truffle + OpenZeppelin to write out our ERC-721 contract to mint Colors 
+We're going to inherit the implemented ERC-721 contract from OpenZeppelin      
+https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/token/ERC721/ERC721Token.sol
+
+As well as their Ownable implementation    
+https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/ownership/Ownable.sol
+
+We already have access to these when we did `npm install` as OpenZeppelin was already a dependency in package.json
+
+In the colors directory, type  
+4. `truffle init` 
+
+Notice that truffle now added 3 directories: `contracts`, `migrations`, and `test`
+
+Open up your favorite IDE (I use VisualStudio with a Solidity plugin) and go into the contracts directory. Let's begin! 
+
+5. Create a new file called `ColorsERC721.sol` in the `contracts` directory    
+6. Create a new file called `ColorsERC721Test.js` in the `test` directory 
+7. Create a new file called `2_migrate_contract.js` in the `migrations` directory 
+
+8. Follow the vide/workshop steps to implement and test your contract, and once you do, the UI included in this tutorial should work with your contract :) 
+
+Workshop slides: https://docs.google.com/presentation/d/11ebTVQnl_0K9I2WWaUjtV5YPdH9yCOga5kqhZdSI308/edit?ts=5a83a50e#slide=id.g30fce2c670_0_0 
+
+Video of the workshop
+https://www.facebook.com/ETHDenver/videos/164729944178073/
+
+Happy hacking! 
---
+++ src/js/app.js
@@ -0,0 +1,124 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  currentColor: null,
+
+  init: function() {
+    App.generateNewColor()
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {d
+      // set the provider you want from Web3.providers
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('ColorsERC721.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var TutorialTokenArtifact = data;
+      App.contracts.TutorialToken = TruffleContract(TutorialTokenArtifact);
+
+      // Set the provider for our contract.
+      App.contracts.TutorialToken.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      return App.getColorForUser();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#mintColors', App.mintColors);
+    $(document).on('click', '#color', App.generateNewColor);
+  },
+
+  generateNewColor: function(event) { 
+    document.getElementById('color').setAttribute('style', 'background-color: ' + App.getRandomColor())
+  },
+
+  mintColors: function(event) {
+    event.preventDefault();
+
+    var colorContractInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+      App.contracts.TutorialToken.deployed().then(function(instance) {
+        colorContractInstance = instance;
+        return colorContractInstance.mint(parseInt(App.currentColor, 16), {from: account, value: new web3.BigNumber(web3.toWei(.001, "ether"))});
+      }).then(function(result) {
+        App.addColorBlock(new web3.BigNumber(parseInt(App.currentColor, 16)))
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  getColorForUser: function() {
+    var colorContractInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.TutorialToken.deployed().then(function(instance) {
+        colorContractInstance = instance;
+        return colorContractInstance.tokensOf(account)
+      }).then((result) => { 
+        result.forEach((bigIntColor) => { 
+          App.addColorBlock(bigIntColor)
+        })
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  addColorBlock: function (bigIntColor) { 
+    let mainelement = document.createElement('div')
+    let textElement = document.createElement('p')
+    mainelement.setAttribute('style', 'display:inline-block;')
+    let element = document.createElement('div')
+    let colorString = "#" + "0".repeat(Math.max(0, 6 - bigIntColor.toNumber().toString(16).length)) + bigIntColor.toNumber().toString(16)
+    textElement.innerText = colorString;
+    element.setAttribute('style', 'background-color:' + colorString)
+    element.setAttribute('class', "color-blocks")
+    mainelement.appendChild(element)
+    mainelement.appendChild(textElement)
+    document.querySelector('#owner-colors').appendChild(mainelement)
+  },
+
+  getRandomColor: function() {
+    var letters = '0123456789ABCDEF';
+    let color = "";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    App.currentColor = color;
+    return "#" + color;
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
